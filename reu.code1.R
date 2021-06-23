4b591ad9e797f82fa445c6895f4ea81686ff6dcc
elliemcdaniel <mcdaniel.e@ufl.edu>
2021-06-23 09:54:21 -0400

    Commentary and issues with looping plants per sample

--- reu.code1.R
+++ reu.code1.R
@@ -1,5 +1,4 @@
 # all packages needed 
-library(purrr)
 library(tidyverse)
 library(mgcv)
 library(gratia)
@@ -8,9 +7,12 @@ library(dplyr)
 
 ha <- read_rds(here("data", "ha_data_subset.rds")) # reading in data
 ha <- ha %>% mutate(spei = spei_history[,1], .before = spei_history) # mutating the spei_history column, creates one that just contains the first value labeled "spei"
+# im not sure if the for loop should go here or line 13
+plants_per_sample <- seq(5000,500,by = -500) # this gets an error for taking too many samples because it isnt looping
+# for (something in plants_per_sample??? maybe) {
 
-pops_to_sample <- 3 # population samples created
-plants_per_sample <- 100 # size of populations
+pops_to_sample <- 1 # population samples created (is one an okay number for this?)
+
 out <- vector("list", pops_to_sample) # create vector to store sample
 
 for (i in 1:pops_to_sample) { # looping sampling process
@@ -43,3 +45,4 @@ summary(m)$r.sq # pulling out r squared value
 r.sq.gam[i] <- summary(m)$r.sq # storing r squared value
 }
 r.sq.gam
+# } i think this is where the loop would end and repeat
